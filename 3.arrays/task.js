54545bf854c67791038ae7c1411b3b0ddb5588f9
Dmitry-A-K <dakobzev@gmail.com>
2023-10-13 00:22:38 +0300

    готовые массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+
+  return arr1.length === arr2.length && arr1.every((element, index) => arr2[index] === element);
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  if(!users || users.length == 0) return 0;
+
+  const usersAge = users.filter(element => element.gender === gender).map(element => element.age);
+  if(usersAge.length == 0) return 0;
+
+  return usersAge.reduce((sum, age) => sum + age, 0)/usersAge.length;
   
 }
